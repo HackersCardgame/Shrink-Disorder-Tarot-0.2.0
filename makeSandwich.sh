86788755210c9e139defcd23be7e68d45bdab8f1
marc <marc@debian>
2017-01-14 02:54:47 +0100

    changed timing from xdotool

--- makeSandwich.sh
+++ makeSandwich.sh
@@ -21,6 +21,18 @@ cd ../scripts
 ./generateA6.sh DE
 ./generateA4.sh EN
 ./generateA6.sh EN
-./scripts/printPDF.sh
+#./scripts/printPDF.sh
 #./printAll.sh DE
 #./printAll.sh EN
+
+echo "
+now you can got to eg the directory assembled/DE/A6 and run ./printAll.sh
+to print your German (DE) cards on A6 Cards on your default printer 
+(select first the correct printer as default, you must be su to do so)
+
+or cd assembled/EN/A4 ./printAll.sh to print all your cards on A4 Sheets
+remember, if you laminate them the look better and are more stabile
+
+have fun...
+"
+
