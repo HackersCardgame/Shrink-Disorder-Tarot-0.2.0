f9d263d78e7656bcb4a8499b798bb5882ff288bb
marcland <marcland@s30.server.hostpoint.ch>
2017-01-16 11:21:03 +0100

    index.html display.php

--- website/display.php
+++ website/display.php
@@ -10,10 +10,18 @@
 <?php
 //mode: list, display, flip
    echo "../".$_GET["dir"]."/*.*<br>";
+   echo "mode=". $_GET["mode"];
    echo "flipping=". $_GET["flipping"];
    echo "display=" . $_GET["display"];
    echo "width=" . $_GET["width"];
    echo "height=" . $_GET["height"]."<br>";
+
+if (empty($_GET["mode"])) {
+ $mode="flip"; 
+}
+else {
+ $mode=$_GET["mode"];
+}
 
 if (empty($_GET["width"])) {
  $width=240; 
@@ -49,13 +57,17 @@ $supported_file = array(
 $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
 if (in_array($ext, $supported_file)) {
 
-if ($_GET["flipping"]!=="false") {
+
+
+if ($mode=="flip") {
  echo '<iframe frameBorder="0" text-align=center height=330 width=240 src="./card.php?front='.$image .'&back=back.jpg"></iframe> ';
 }
 
-if ($_GET["flipping"]=="false") {
- echo '<a href="'.$image.'" target="_blank"><font color=red><b>'.$image.'</b></font>';}
-if ($_GET["display"]=="true") {
+if ($mode=="list") {
+ echo '<br><a href="'.$image.'" target="_blank"><font color=red><b>'.$image.'</b></font>';
+}
+
+if ($mode=="display") {
  echo  '<iframe frameBorder="0" text-align=center height='.$height.' width='.$width.' src="'.$image.'"></iframe></a>';
 }
 
