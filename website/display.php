f2193b0db812866c85bb3f18cfeaf74695f848df
marcland <marcland@s30.server.hostpoint.ch>
2017-01-16 10:11:13 +0100

    index.html DE modified

--- website/display.php
+++ website/display.php
@@ -10,6 +10,7 @@
 <?php
 
    echo "../".$_GET["dir"]."/*.*<br>";
+   echo "flipping=". $_GET["flipping"]."<br>";
 
    $files = glob("../".$_GET["dir"]."/*.*");
 
@@ -23,6 +24,7 @@ $supported_file = array(
     'jpg',
     'jpeg',
     'png',
+    'pdf',
     'svg'
 );
 
@@ -31,7 +33,14 @@ if (in_array($ext, $supported_file)) {
 //    print $image ."<br />";
 
 //    echo '<a href="./'.$image .'" target=_blank><img style="margin:5px" width=250 src="'.$image .'" ></a>';
+
+if ($_GET["flipping"]!=="false") {
  echo '<iframe frameBorder="0" text-align=center height=330 width=240 src="./card.php?front='.$image .'&back=back.jpg"></iframe> ';
+}
+
+if ($_GET["flipping"]=="false") {
+ echo '<iframe frameBorder="0" text-align=center height=330 width=240 src="'.$image.'"></iframe> ';
+}
 
 } else {
     continue;
