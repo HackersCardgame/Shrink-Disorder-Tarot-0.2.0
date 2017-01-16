db85f9e489475d3f79ead3f203241221e61bae31
marcland <marcland@s30.server.hostpoint.ch>
2017-01-16 11:06:13 +0100

    index.html display.php

--- website/display.php
+++ website/display.php
@@ -8,10 +8,27 @@
   <body bgcolor="#FFF">
 
 <?php
-
+//mode: list, display, flip
    echo "../".$_GET["dir"]."/*.*<br>";
-   echo "flipping=". $_GET["flipping"]."<br>";
-   echo "display=" . $_GET["display"]."<br>";
+   echo "flipping=". $_GET["flipping"];
+   echo "display=" . $_GET["display"];
+   echo "width=" . $_GET["width"];
+   echo "height=" . $_GET["height"]."<br>";
+
+if (empty($_GET["width"])) {
+ $width=240; 
+}
+else {
+ $height=$_GET["width"];
+}
+
+if (empty($_GET["height"])) {
+ $height=330; 
+}
+else {
+ $height=$_GET["height"];
+}
+
 
    $files = glob("../".$_GET["dir"]."/*.*");
 
@@ -31,18 +48,15 @@ $supported_file = array(
 
 $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
 if (in_array($ext, $supported_file)) {
-//    print $image ."<br />";
-
-//    echo '<a href="./'.$image .'" target=_blank><img style="margin:5px" width=250 src="'.$image .'" ></a>';
 
 if ($_GET["flipping"]!=="false") {
  echo '<iframe frameBorder="0" text-align=center height=330 width=240 src="./card.php?front='.$image .'&back=back.jpg"></iframe> ';
 }
 
 if ($_GET["flipping"]=="false") {
- echo '<a href="'.$image.'" target="_blank"><font color=red><b>'.$image.'</b></font><br>';}
+ echo '<a href="'.$image.'" target="_blank"><font color=red><b>'.$image.'</b></font>';}
 if ($_GET["display"]=="true") {
- echo  '<iframe frameBorder="0" text-align=center width=90% src="'.$image.'"></iframe></a><br> ';
+ echo  '<iframe frameBorder="0" text-align=center height='.$height.' width='.$width.' src="'.$image.'"></iframe></a>';
 }
 
 } else {
