9ca89cd33b0f5096d3abc79635241d304ecdb5e9
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2017-01-18 01:35:10 +0100

    tempsave A6 back-side svg

---
+++ configPrinterOffset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if  [ "$1" = "" ]
+then
+echo -e "
+\e[39m
+Usage:
+------
+to configure the printer offset for the rear-side edit: \e[36m./back/back_A6.svg or ./back/back_A4.svg\e[39m
+
+"
+exit 0
+fi
+
+
