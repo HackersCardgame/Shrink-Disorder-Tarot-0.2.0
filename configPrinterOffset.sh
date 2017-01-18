d72695ee23f06d50f3cbfad071b2318b9ef410f7
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2017-01-18 02:41:22 +0100

    tempsave A6 back-side svg

--- configPrinterOffset.sh
+++ configPrinterOffset.sh
@@ -3,16 +3,36 @@
 if  [ "$1" = "" ]
 then
 echo -e "
-\e[39m
 Usage:
 ------
-to configure the printer offset for the rear-side edit: \e[36m./template/back_A6.svg or ./template/back_A4.svg\e[0m
+to configure the printer offset for the rear-side edit: \e[36m./template/back_\e[31mA6\e[36m.svg\e[0m or \e[36m./template/back_\e[31mA4\e[36m.svg\e[0m
 if you want to use the template for the Canon Photo Paper A6 just copy this file to back_A6.svg
 
-(my system is configured with the \e[33mmetric system\e[0m and i use \e[33mDIN A4\e[0m, \e[31mUS Letter System not test yet\e[0m...)
+Example:
+--------
+\e[33m./configurePrinter.sh\e[31m A6\e[0m
+\e[31m(not input sanitized, case sensitive)\e[0m
 
+
+(my system is configured with the \e[33mmetric system\e[0m and i use \e[33mDIN A4\e[0m, \e[31mUS Letter System not test yet\e[0m...)
 "
 exit 0
 fi
 
+echo -e "
+
+to edit the current template press \e[1m[ENTER]\e[0m or copy a given template to ./template/back_A6.svg
+
+this script will also open the first other card in the EN directory, so you can print both sides of the test card sheet
+
+"
+read
+
+
+
+/usr/bin/inkscape ./template/back_$1.svg &
+
+find ./assembled/EN/$1 -name "*-*.svg" >./cards.txt
+
+/usr/bin/inkscape $(head ./cards.txt -n1) &
 
