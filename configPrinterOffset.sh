9f67861ccfd6d630f51f259482582de1262f6384
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2017-01-18 01:43:46 +0100

    tempsave A6 back-side svg

--- configPrinterOffset.sh
+++ configPrinterOffset.sh
@@ -6,7 +6,10 @@ echo -e "
 \e[39m
 Usage:
 ------
-to configure the printer offset for the rear-side edit: \e[36m./back/back_A6.svg or ./back/back_A4.svg\e[39m
+to configure the printer offset for the rear-side edit: \e[36m./template/back_A6.svg or ./template/back_A4.svg\e[0m
+if you want to use the template for the Canon Photo Paper A6 just copy this file to back_A6.svg
+
+(my system is configured with the \e[33mmetric system\e[0m and i use \e[33mDIN A4\e[0m, \e[31mUS Letter System not test yet\e[0m...)
 
 "
 exit 0
