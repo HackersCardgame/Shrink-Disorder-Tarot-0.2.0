96988e96e42bef7dba898f35abb65a720fd3aec5
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2017-01-19 01:39:35 +0100

    patternRecog

--- scripts/QRcode.sh
+++ scripts/QRcode.sh
@@ -7,3 +7,4 @@ sudo apt-get install qrencode
 
 qrencode -t svg -o dev.svg https://github.com/braindef/Shrink-Disorder-Tarot-0.2.0
 qrencode -t svg -o website.svg http://marclandolt.ch/Shrink-Disorder-Tarot-0.2.0
+qrencode -t svg -o retailSurveillance.svg https://media.ccc.de/v/33c3-8238-retail_surveillance_retail_countersurveillance#video&t=476
